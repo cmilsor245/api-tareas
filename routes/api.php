64b467e4855b7658a7c0ce4594dddfaf72e5c652
cmilsor245 <cmilsor245@g.educaand.es>
2024-02-23 01:10:30 +0100

    adding api route for tareas controller basic configuration

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TareaController;
 
 Route::middleware('auth:sanctum') -> get('/user', function (Request $request) {
   return $request -> user();
 });
+
+Route::resource('/tareas', TareaController::class);
