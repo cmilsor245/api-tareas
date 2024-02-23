0d174d29ace10128f9ff35c8c2b69c4e656520e8
cmilsor245 <cmilsor245@g.educaand.es>
2024-02-23 01:29:36 +0100

    adding an api route for etiquetas

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,12 @@
 <?php
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TareaController;
+use App\Http\Controllers\EtiquetaController;
 
 Route::middleware('auth:sanctum') -> get('/user', function (Request $request) {
   return $request -> user();
 });
 
 Route::resource('/tareas', TareaController::class);
+Route::resource('/etiquetas', EtiquetaController::class);
