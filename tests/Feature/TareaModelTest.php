e88b650fda3c4eb9e1346ec6db95a6d3ca5de910
cmilsor245 <cmilsor245@g.educaand.es>
2024-02-27 19:50:19 +0100

    creating first model test

---
+++ tests/Feature/TareaModelTest.php
@@ -0,0 +1,17 @@
+<?php
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TareaModelTest extends TestCase {
+  /**
+   * a basic feature test example
+   */
+  public function test_example(): void {
+    $response = $this -> get('/');
+
+    $response -> assertStatus(200);
+  }
+}
