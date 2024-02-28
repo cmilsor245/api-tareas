c1b36e9c3263aeb0e7bc76103d79a4782137ec59
cmilsor245 <cmilsor245@g.educaand.es>
2024-02-28 16:45:46 +0100

    configuring auto user authentication and creating 3 first tests

--- tests/Feature/TareaModelTest.php
+++ tests/Feature/TareaModelTest.php
@@ -4,14 +4,46 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
+use App\Models\Tarea;
 
 class TareaModelTest extends TestCase {
-  /**
-   * a basic feature test example
-   */
-  public function test_example(): void {
-    $response = $this -> get('/');
+  use RefreshDatabase;
 
-    $response -> assertStatus(200);
+  public function setUp(): void {
+    parent::setUp();
+    $this -> actingAs(User::factory() -> create());
+  }
+
+  /* ----------------------------------------------------------------------------------------------- */
+
+  public function test_se_crea_una_tarea_con_titulo_y_descripcion() {
+    $tarea = new Tarea();
+    $tarea -> titulo = 'test';
+    $tarea -> descripcion = 'test';
+    $tarea -> save();
+
+    $this -> assertDatabaseHas('tareas', [
+      'titulo' => 'test',
+      'descripcion' => 'test'
+    ]);
+  }
+
+  public function test_se_crea_una_tarea_solo_con_titulo() {
+    $tarea = new Tarea();
+    $tarea -> titulo = 'test';
+    $tarea -> save();
+
+    $this -> assertDatabaseHas('tareas', [
+      'titulo' => 'test'
+    ]);
+  }
+
+  public function test_no_se_puede_crear_una_tarea_sin_titulo() {
+    $response = $this -> postJson('/api/tareas', [
+      'descripcion' => 'test'
+    ]);
+
+    $response -> assertStatus(422);
   }
 }
